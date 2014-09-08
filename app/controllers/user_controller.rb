96662064b5ca7e01128167ca040a96c409a0145d
Thomsen <changsen4584@163.com>
2014-09-08 23:05:25 +0800

    create user in table

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,17 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :loginname, :password)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :loginname, presence: true,
+                        length: {minimum: 4}
+  validates :password, presence: true,
+                       length: {minimum: 6, maximum: 12}
+  
 end
