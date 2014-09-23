aa9e761bd83e017ce02db86d9523c1d36ab7d7e4
Thomsen <changsen4584@163.com>
2014-09-23 21:56:03 +0800

    login controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,35 @@ class WelcomeController < ApplicationController
   def index
     @user = User.new
   end
+
+  def main
+    
+  end
+
+  def login
+    unless request.get?
+      username = params[:user][:username]
+      password = params[:user][:password]
+      @hint = "ok"
+      @user = User.find_by_username(username)
+      if @user.class == NilClass
+        @hint = 'unregisted username!'
+        redirect_to welcome_index_path
+      elsif @user.password == password
+        @hint = 'welcome back'
+        redirect_to welcome_main_path
+      else
+        @hint = 'login failed, please check up you info'
+      end
+      
+    end
+    
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'welcome/register'
   get 'welcome/login'
+  post 'welcome/login'
   get 'welcome/index'
+  get 'welcome/main'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
