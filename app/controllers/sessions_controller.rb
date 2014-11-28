ff31dfb2af76c4b50759c5b27881b226a6b79863
Thomsen <changsen4584@163.com>
2014-11-28 23:53:31 +0800

    user session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authen_by_username(params[:session][:username], params[:session][:password])
+
+    if user.nil?
+      flash.now[:error] = "Invalid username/password combination"
+      render :new
+    else
+      sign_in user
+      redirect_to mains_index_path
+    end
+    
   end
 
   def destroy
   end
+
+  private
+  def user_params
+    params(:session).permit(:username, :form_password)
+  end
+  
 end
